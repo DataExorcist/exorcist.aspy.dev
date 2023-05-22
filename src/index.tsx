7c0eda539e40b76ad56862e7b8a37e44eff01775
Aiden <amb@aspy.dev>
2023-05-21 22:03:42 -0400

    I think we are done with this

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-/* @refresh reload */
 import { render } from 'solid-js/web';
-
+import Logo from './exorcist.svg?component-solid'
 import './index.css';
-import App from './App';
 
-const root = document.getElementById('root');
-
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got mispelled?',
-  );
-}
-
-render(() => <App />, root!);
+const Home = () => {
+    return (
+        <>
+        <Logo fill="#880808" class="logo"/>
+            <h1 class="title">E X O R C I S T</h1>
+            <p class="description">Exorcist is a script to exorcise your data from data hoarding companies<br/>Many companies scrape, sell and exchange your data- but you can opt out!</p>
+            <p class="how">How is this accomplished? With your email, notices are sent to a list of data brokers telling them to remove you<br />from their lists. This uses privacy laws like the GPDR and the CCPA (commonly known as the California residents opt out)</p>
+            <p class="how"><a href="https://github.com/DataExorcist/Exorcist">View the repository on GitHub!</a></p>
+            <footer>&copy Aiden 2023 - <a href='https://aspy.dev'>aspy.dev</a></footer>
+        </>
+    )
+};
+render(() => <Home />, document.getElementById('root') as Element);
